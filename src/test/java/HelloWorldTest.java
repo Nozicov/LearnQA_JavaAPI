59f9ed7426eca1ec4a1812a7e60b00da640818ac
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-13 21:12:50 +0600

    Added testGetHello and Ex3

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello World!");
+    public void testHello(){
+        System.out.println("Hello from NEE");
     }
+
+    @Test
+    public void testGetHello(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
 }
