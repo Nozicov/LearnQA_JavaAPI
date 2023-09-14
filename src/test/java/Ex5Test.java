069363e3d09ed352717a0399296181b3ce9c9520
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-14 15:48:50 +0600

    Clear imports

--- src/test/java/Ex5Test.java
+++ src/test/java/Ex5Test.java
@@ -2,9 +2,6 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Ex5Test {
 
     @Test
