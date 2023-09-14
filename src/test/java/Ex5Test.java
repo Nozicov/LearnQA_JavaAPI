e79dbf1909bf67a080e84d0310d1b68b2d5b2036
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-14 15:47:35 +0600

    Ex5: Парсинг JSON

---
+++ src/test/java/Ex5Test.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex5Test {
+
+    @Test
+    public void testGetJsonHomeWork(){
+        JsonPath resoponse = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        System.out.println("Response:");
+        resoponse.prettyPrint();
+
+        System.out.println("Text message two:");
+        String message = resoponse.get("messages.message[1]").toString();
+        System.out.println(message);
+    }
+}
