77d829c615374e6b156d61f522a3104abaccf141
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-27 20:04:07 +0600

    Ex10: Тест на короткую фразу

---
+++ src/test/java/Ex10Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex10Test {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"15CharacterText", "16_CharacterText", "5Text"})
+    public void testLenghText(String text){
+        assertTrue(text.length() > 15, "Length text not 15 characters");
+    }
+}
