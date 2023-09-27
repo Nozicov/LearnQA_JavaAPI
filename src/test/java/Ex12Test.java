a6b3fe30f4705ded21a6d7d7dcab8ab0598a739c
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-28 01:28:10 +0600

    Ex12: Тест запроса на метод header

---
+++ src/test/java/Ex12Test.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex12Test {
+
+    @Test
+    public void testGetHeader(){
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
+        assertEquals(200, response.statusCode(), "Service response code is not equal to 200");
+        assertTrue(responseHeaders.size() > 0, "Not headers");
+        assertEquals(responseHeaders.getValue("x-secret-homework-header"), "Some secret value", "Header x-secret-homework-header value is not equal to x-secret-homework-header");
+
+    }
+}
