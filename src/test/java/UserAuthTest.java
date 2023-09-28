3233f585c5ade7eacda6c75e0ca1e0e6395e2a59
Yevgeniy Nozikov <yevgeniy.nozikov@aitupay.kz>
2023-09-29 05:55:52 +0600

    III - 04. Негативные тесты на авторизацию

--- src/test/java/UserAuthTest.java
+++ src/test/java/UserAuthTest.java
@@ -2,7 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,13 +14,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserAuthTest {
+        Map<String, String> authData = new HashMap<>();
 
     @Test
     public void testAuthUser(){
-        Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
-
         Response responseGetAuth = RestAssured
                 .given()
                 .body(authData)
@@ -48,5 +50,34 @@ public class UserAuthTest {
         assertEquals(userIdCheck, userIdOnAuth);
 
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testNegativeAuthUser(String condition){
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String,String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+        if(condition.equals("cookie")){
+            spec.cookies("auth_sid", cookies.get("auth_sid"));
+        } else if (condition.equals("headers")) {
+            spec.header("x-csrf-token", headers.get("x-csrf-token"));
+        } else {
+            throw new IllegalArgumentException("Codition value is known: " + condition);
+        }
+
+        JsonPath responseForCheck = spec.get().jsonPath();
+        assertEquals(0, responseForCheck.getInt("user_id"), "user_id not equals 0");
     }
 }
